929c4f5e3f8f5ed080bfb683fb496a96a628dcfb
P-rateek-S-ingh <142905214+P-rateek-S-ingh@users.noreply.github.com>
2024-02-26 09:11:42 +0000

    conversionOperations

---
+++ Basics/ConversionsOperations.js
@@ -0,0 +1,30 @@
+let value = 0;
+
+let valueLogedIn = Number(value);
+
+let booleanLogedIn = Boolean(value);
+
+// console.log(valueLogedIn)
+
+// console.log(booleanLogedIn)
+
+let logedIn = "Prateek"
+
+let conversion = Number(logedIn)
+let checking = Boolean(logedIn)
+
+// console.table([conversion,checking,valueLogedIn,booleanLogedIn])
+
+let number = 78
+let stringnumber = String(number)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+
+//*******************OPERATIONS***********************
+
+console.log(1+2)
+console.log("1"+2)
+console.log(1+"2");
+console.log(1+2+"3")
+console.log("1"+2+3);
