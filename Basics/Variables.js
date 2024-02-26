fb24655adf988e31dd63a3c51c37aee137d0e863
P-rateek-S-ingh <142905214+P-rateek-S-ingh@users.noreply.github.com>
2024-02-26 08:22:22 +0000

    datatypes

---
+++ Basics/Variables.js
@@ -0,0 +1,5 @@
+console.log("Hello world");
+let a = "welcome to Jhansi";
+const b = "welcom to git"
+let accountState;
+console.table([a,accountState,b]);
---
+++ Basics/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //trate all js code as newer version
+let a = "hello world"
+let b = 45
+let c;
+let d = false;
+//number
+//bigint
+//"string"
+//boolean
+//null
+//undefined
+//symbol
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof null);
