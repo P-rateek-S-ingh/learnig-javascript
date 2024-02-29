df037e8636215f5bb2f580f1c8284fb08597cc1f
Prateek singh <142905214+P-rateek-S-ingh@users.noreply.github.com>
2024-02-29 18:23:49 +0000

    functions

--- Basics3/functions.js
+++ Basics3/functions.js
@@ -2,14 +2,24 @@
 
 
 
-function MyName(){
-    console.log("P")
-    console.log("R")
-    console.log("A")
-    console.log("T")
-    console.log("E")
-    console.log("E")
-    console.log("K")
+// function MyName(){
+//     console.log("P")
+//     console.log("R")
+//     console.log("A")
+//     console.log("T")
+//     console.log("E")
+//     console.log("E")
+//     console.log("K")
+// }
+
+// MyName()
+
+
+// functions to add two numbers
+
+function AddTwoNumbers(num1,num2){
+    console.log(num1 + num2);
 }
 
-MyName()
+AddTwoNumbers(13 , 12)
+
