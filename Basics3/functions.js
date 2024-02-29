5d0e3608bf283dadf99056a95bd89486a37cbf32
Prateek singh <142905214+P-rateek-S-ingh@users.noreply.github.com>
2024-02-29 18:56:11 +0000

    functions and if condition

--- Basics3/functions.js
+++ Basics3/functions.js
@@ -17,9 +17,28 @@
 
 // functions to add two numbers
 
+// function AddTwoNumbers(num1,num2){
+//     console.log(num1 + num2);
+// }
+
 function AddTwoNumbers(num1,num2){
-    console.log(num1 + num2);
+   return(num1 + num2)
 }
 
-AddTwoNumbers(13 , 12)
+// console.log(`Result:`, AddTwoNumbers(13 , 12));  
 
+
+function UserLoginMessage(UserName){
+    return `${UserName}, just logged in...`
+}
+// console.log(UserLoginMessage("Aditya"));
+
+
+function instaUser(Username = "sam"){
+if (!Username) {
+     return `Please enter the name`
+}
+    return `${Username}, just loggeg in...`
+}
+
+console.log(instaUser("Prateek"));
