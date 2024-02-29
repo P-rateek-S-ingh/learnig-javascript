30d0b8d1bf36e289a59d979edb824dc3667bffb7
Prateek singh <142905214+P-rateek-S-ingh@users.noreply.github.com>
2024-02-29 18:18:56 +0000

    functions

---
+++ Basics3/functions.js
@@ -0,0 +1,15 @@
+//functions
+
+
+
+function MyName(){
+    console.log("P")
+    console.log("R")
+    console.log("A")
+    console.log("T")
+    console.log("E")
+    console.log("E")
+    console.log("K")
+}
+
+MyName()
