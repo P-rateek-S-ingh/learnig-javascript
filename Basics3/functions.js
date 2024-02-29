a2ee40a951eb5530a155460723d24d042e193f0a
Prateek singh <142905214+P-rateek-S-ingh@users.noreply.github.com>
2024-02-29 19:42:40 +0000

    handeling objects and array though functions

--- Basics3/functions.js
+++ Basics3/functions.js
@@ -36,4 +36,67 @@ function instaUser(Username = "sam") {
   return `${Username}, just loggeg in...`;
 }
 
-console.log(instaUser("Prateek"));
+// console.log(instaUser("Prateek"));
+
+// calculate the the  cost of shopping cart
+
+// function calculateCart(...num1) {
+//   let Total = 0
+//   for (let i = 0; i < num1.length; i++) {
+//     Total = Total + num1[i];
+//   }
+//   return Total;
+// }
+
+// console.log(calculateCart(100, 200, 400));
+
+
+// function calculateCart(...num1) {
+//     return num1
+// }
+
+// console.log(calculateCart(100,200, 300));
+
+
+function calculateCart(val1 ,val2 ,...num1) {
+  return num1
+}
+
+// console.log(calculateCart(100,200, 300, 500));
+
+//handleing objects   ---> 1 way
+
+const user ={
+  usernAme:'Parv',
+  price:349
+}
+
+function handleObject(anyobject){
+  // console.log(`The user name is '${anyobject.usernAme}' and the price of course is '${anyobject.price} INR'`);
+
+}
+// handleObject(user)
+
+
+  // ----->   2nd Way
+
+function handleObject(anyobject){
+  console.log(`The user name is '${anyobject.username}' and the price of course is '${anyobject.coursePrice} INR'`);
+
+}
+// handleObject({
+//   username:'Ashish',
+//   coursePrice:1299
+// })
+
+
+//hadeling Arrays
+
+const newArray = [100 , 200, 800, 200]
+
+function returnThirdvalue(Getvalue){
+  return Getvalue[2]
+}
+
+// console.log(returnThirdvalue(newArray));
+console.log(returnThirdvalue([100 , 200, 600, 200]));
