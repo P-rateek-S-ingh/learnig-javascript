6061a5dc7d22f57092d63eb966eeb68ea05adfef
Prateek singh <142905214+P-rateek-S-ingh@users.noreply.github.com>
2024-03-04 20:44:27 +0000

    while loop

---
+++ Iterations/While_loop.js
@@ -0,0 +1,22 @@
+//While loop
+
+let index = 0
+
+while(index <= 10){
+    // console.log(`Value of index: ${index}`);
+    index += 2
+}
+
+let arr = ["Ironman","Batman", "saktiman","Hatim tai"]
+let i = 0
+while ( i  < arr.length) {
+    // console.log(`Array Elements are: ${arr[i]}`);
+    i++
+}
+
+
+let score = 12
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score <= 10)
