503235fc00ebb8e092999212d04a9bf24804e50f
Prateek singh <142905214+P-rateek-S-ingh@users.noreply.github.com>
2024-03-05 19:09:43 +0000

    map method

---
+++ Iterations/Map_methods.js
@@ -0,0 +1,8 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let UpdatedVal = nums.map((items) => items + 10)
+// let UpdatedVal = nums.map((items) => items + 10).map((items) => items + 100 ).map((items) => items * 3).map((items) => items + 1 ).filter((items) => items > 340) //chaining method
+// console.log(UpdatedVal);
+
+
+
+
